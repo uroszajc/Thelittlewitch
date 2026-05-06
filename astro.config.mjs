fbabaa32da3b57a37f38abaad72d1410fb8938b0
Kit <kit@thelittlewitch.com.au>
2026-05-06 06:12:47 +0000

    Revert "Update site URL to www2.thelittlewitch.com.au"
    This reverts commit dcbc0e2c7576c105064bf6948d8b1bfaa2095bfd.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://www2.thelittlewitch.com.au',
+  site: 'https://thelittlewitch.com.au',
   base: '/',
   build: {
     outDir: './dist'
