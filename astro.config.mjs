dcbc0e2c7576c105064bf6948d8b1bfaa2095bfd
Kit <kit@thelittlewitch.com.au>
2026-05-06 05:57:25 +0000

    Update site URL to www2.thelittlewitch.com.au

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://thelittlewitch.com.au',
+  site: 'https://www2.thelittlewitch.com.au',
   base: '/',
   build: {
     outDir: './dist'
